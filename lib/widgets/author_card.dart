e6ad93074f6ec40e4a7652ab7f04ef00abd9e546
ElhamElshami <elhamelshami.dev@gmail.com>
2022-03-05 10:58:49 +0300

    Added card 2

--- lib/widgets/author_card.dart
+++ lib/widgets/author_card.dart
@@ -13,6 +13,7 @@ class AuthorCard extends StatelessWidget {
     return Container(
       padding: const EdgeInsets.all(16),
       child: Row(
+         mainAxisAlignment:MainAxisAlignment.spaceBetween,
         children:  [
            Row(
              children: [
--- lib/widgets/card2.dart
+++ lib/widgets/card2.dart
@@ -1,6 +1,8 @@
 import 'package:challenge/widgets/author_card.dart';
 import 'package:flutter/material.dart';
 
+import '../theme/fooderlichTheme.dart';
+
 class Card2 extends StatelessWidget {
   const Card2({Key? key}) : super(key: key);
 
@@ -13,7 +15,38 @@ class Card2 extends StatelessWidget {
         image: DecorationImage(image: AssetImage('assets/bac2.jpg'),fit: BoxFit.cover),
         borderRadius: BorderRadius.all(Radius.circular(10.0))
       ),
-      child: const AuthorCard(authorName: 'Mike Katz',title: 'Smoothie Connoisseur',imageProvider:NetworkImage('https://freerangestock.com/sample/116135/handsome-man-avatar-.jpg'))
+      child:  Column(
+        children:  [
+          const AuthorCard(authorName: 'Mike Katz',title: 'Smoothie Connoisseur',imageProvider:NetworkImage('https://freerangestock.com/sample/116135/handsome-man-avatar-.jpg')),
+          Expanded(
+ child: Stack(
+ children: [
+ Positioned(
+ bottom: 16,
+ right: 16,
+ child: Text(
+ 'Recipe',
+ style: FooderlichTheme.lightTextTheme.headline1,
+ ),
+ ),
+
+ Positioned(
+ bottom: 70,
+ left: 16,
+ child: RotatedBox(
+ quarterTurns: 3,
+ child: Text(
+ 'Smoothies',
+ style: FooderlichTheme.lightTextTheme.headline1,
+ ),
+ ),
+ ),
+ ],
+ ),
+        
+ ),
+        ]
+      )
     );
   }
 }
