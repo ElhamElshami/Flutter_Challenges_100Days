a183c210d0672ee4ad4709fad6d184da0e8b8051
ElhamElshami <elhamelshami.dev@gmail.com>
2022-03-24 18:52:35 +0300

    Update Files

--- lib/Screens/status_screen.dart
+++ lib/Screens/status_screen.dart
@@ -1,16 +1,56 @@
 import 'package:flutter/material.dart';
 
-class StatusScreen extends StatefulWidget {
+class StatusScreen extends StatelessWidget {
 
-  @override
-  State<StatusScreen> createState() => _StatusScreenState();
-}
-
-class _StatusScreenState extends State<StatusScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child:Text('STATUS SCREEN') ,
+    return Container(
+      color: Colors.grey[300],
+      child: Card(
+        child: ListTile(
+          leading:Stack(
+            children: [
+              Container(
+                padding: const EdgeInsets.all(5),
+                decoration:   BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(
+                    color: Colors.grey,
+                    width: 2,
+                  ),
+                  borderRadius: BorderRadius.circular(50),
+                ),
+                child: CircleAvatar(
+                   child: Image.network("https://s3.amazonaws.com/wll-community-production/images/no-avatar.png")
+                 ),
+              ),
+               Positioned(
+                 bottom: 0,
+                 right: 0,
+                 child: Container(
+                   decoration: BoxDecoration(
+                     borderRadius: BorderRadius.circular(50),
+                     color: Colors.green
+                   ),
+                   child: const Icon(Icons.add,color: Colors.white,size: 18,),
+                 ),
+               )
+            ],
+          ) ,
+           title: const Text(
+             'My Status',
+             style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 18
+             ),
+           ),
+           subtitle: const Text('Tap to add status update',
+           style: TextStyle(
+             fontSize:15.0
+           ),
+           ),
+        ),
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,11 @@ import 'package:challenge/whatsapp_home.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const WhatsApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class WhatsApp extends StatelessWidget {
+  const WhatsApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
@@ -14,9 +14,11 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'WhatsApp Clone',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primaryColor: const Color(0xff075E54), 
+        colorScheme: ColorScheme.fromSwatch().copyWith(secondary: const Color(0xff25D366)),
       ),
       home:WhatsAppHome(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
